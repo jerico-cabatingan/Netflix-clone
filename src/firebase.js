00639fab35829b4513167f569bbad85a6ef0af36
Jerico <jerico.cabatingan@gmail.com>
2023-08-22 11:53:53 +0100

    Initialsing app

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAg2LxAWApkE37cRWct0tOQm-pn7waRCrk",
+  authDomain: "netflix-clone-24f4c.firebaseapp.com",
+  projectId: "netflix-clone-24f4c",
+  storageBucket: "netflix-clone-24f4c.appspot.com",
+  messagingSenderId: "808020978734",
+  appId: "1:808020978734:web:c420d634a7a956f0ae83bf"
+};
