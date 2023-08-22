275a7b130876d5f35fb6570da13dcd4d0a50822b
Jerico <jerico.cabatingan@gmail.com>
2023-08-22 22:43:00 +0100

    populated and styled Row components

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import "./HomeScreen.css";
 import Nav from "./Nav";
 import Banner from './Banner';
+import Row from './Row';
+import requests from './requests';
 
 function HomeScreen() {
   return (
@@ -10,7 +12,14 @@ function HomeScreen() {
 
       <Banner />
 
-      {/* {Row} */}
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorroMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
     </div>
   )
 }
