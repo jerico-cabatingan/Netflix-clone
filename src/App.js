c84272c7097d196fd48f239a2b935f280aacddd7
Jerico <jerico.cabatingan@gmail.com>
2023-08-23 11:34:45 +0100

    Deployed on firebase and refactored directory structure to include react routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import HomeScreen from './HomeScreen';
-
+import HomeScreen from './screens/HomeScreen';
 import './App.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route exact path="/" element={<HomeScreen/>}/>
+    </Route>
+  )
+);
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen /> 
+      <RouterProvider router={router} />
     </div>
   );
 }
